e97fb5758dece0231444784455efd3b5ef2456f4
Osulliv6 <osulliv6@purdue.edu>
2023-03-21 16:01:24 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,19 @@
-//test
+#include <LSS.h>
+#define LSS_ID		(0) //sets the default LSS ID to 0 
+#define LSS_BAUD	(LSS_DefaultBaud) //change for the serial port for the platform 
+#define LSS_SERIAL	(Serial)
+LSS myLSS = LSS(LSS_ID); //creates an LSS obj
+
+void setup() {  
+  // put your setup code here, to run once:
+  bool toggle = false; 
+  bool fold_in = false; 
+  bool left = false; 
+  bool right = false; 
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
